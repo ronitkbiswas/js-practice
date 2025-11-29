12eb97819981bf412f4b846d65bd93fd359f4788
Ronit <ronitkbiswas@gmail.com>
2025-11-30 00:10:12 +0530

    changed a few things

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function show() {
-  document.getElementById("id1").style.maxHeight = "200px";
+  document.getElementById("id1").style.maxHeight = "300px";
   var images = document.querySelectorAll("#id1 img");
   for (var i = 0; i < images.length; i++) {
     images[i].src = images[i].getAttribute("data-src");
