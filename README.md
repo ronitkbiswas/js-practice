1fc30dd93dc62d4f95c81fbc799e76bd9a2ab353
Ronit <ronitkbiswas@gmail.com>
2025-11-29 21:24:22 +0530

    updated everything from groundup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+On a Click of a button this will generate a random image from the web.
 find live link here
 https://ronitkbiswas.github.io/js-practice/
--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// const random_id = Math.trunc(Math.random() * 12 + 1);
-// const btn = document.createElement("button");
-// const p = document.createElement("p");
-
-// btn.textContent = "Generate Instantly";
-// btn.addEventListener("click", function () {
-//   location.reload();
-// });
-
-// const names = [
-//   "sujoy",
-//   "mihir",
-//   "jhuma",
-//   "supratik",
-//   "ritu",
-//   "unnati",
-//   "reshma",
-//   "suparna",
-//   "bappa",
-//   "santosh",
-//   "santu",
-//   "kanika",
-//   "masi",
-// ];
-
-// document.body.append(
-//   "random_id: " + random_id,
-//   document.createElement("br"),
-//   "picked names: " + names[random_id],
-//   document.createElement("br"),
-//   document.createElement("br"),
-//   btn
-// );
-
 function show() {
   document.getElementById("id1").style.maxHeight = "200px";
   var images = document.querySelectorAll("#id1 img");
