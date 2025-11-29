3c8e52dd42ece89c0c3a768531aff7474b45fdc8
Ronit <ronitkbiswas@gmail.com>
2025-11-29 20:42:53 +0530

    made huge groundup changes

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-const random_id = Math.trunc(Math.random() * 12 + 1);
-const btn = document.createElement("button");
-const p = document.createElement("p");
+// const random_id = Math.trunc(Math.random() * 12 + 1);
+// const btn = document.createElement("button");
+// const p = document.createElement("p");
 
-btn.textContent = "Generate Instantly";
-btn.addEventListener("click", function () {
-  location.reload();
+// btn.textContent = "Generate Instantly";
+// btn.addEventListener("click", function () {
+//   location.reload();
+// });
+
+// const names = [
+//   "sujoy",
+//   "mihir",
+//   "jhuma",
+//   "supratik",
+//   "ritu",
+//   "unnati",
+//   "reshma",
+//   "suparna",
+//   "bappa",
+//   "santosh",
+//   "santu",
+//   "kanika",
+//   "masi",
+// ];
+
+// document.body.append(
+//   "random_id: " + random_id,
+//   document.createElement("br"),
+//   "picked names: " + names[random_id],
+//   document.createElement("br"),
+//   document.createElement("br"),
+//   btn
+// );
+
+const userName = document.getElementById("userName");
+const submitBtn = document.querySelector("#btn");
+
+submitBtn.addEventListener("click", function () {
+  const enteredValue = userName.value;
+  alert(enteredValue);
 });
-
-const names = [
-  "sujoy",
-  "mihir",
-  "jhuma",
-  "supratik",
-  "ritu",
-  "unnati",
-  "reshma",
-  "suparna",
-  "bappa",
-  "santosh",
-  "santu",
-  "kanika",
-  "masi",
-];
-
-document.body.append(
-  "random_id: " + random_id,
-  document.createElement("br"),
-  "picked names: " + names[random_id],
-  document.createElement("br"),
-  document.createElement("br"),
-  btn
-);
