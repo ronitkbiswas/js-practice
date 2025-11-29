2834318719ee995c878c0b138855c049014f854f
Ronit <ronitkbiswas@gmail.com>
2025-11-29 21:16:21 +0530

    first commit

--- script.js
+++ script.js
@@ -32,10 +32,18 @@
 //   btn
 // );
 
-const userName = document.getElementById("userName");
-const submitBtn = document.querySelector("#btn");
-
-submitBtn.addEventListener("click", function () {
-  const enteredValue = userName.value;
-  alert(enteredValue);
-});
+function show() {
+  document.getElementById("id1").style.maxHeight = "200px";
+  var images = document.querySelectorAll("#id1 img");
+  for (var i = 0; i < images.length; i++) {
+    images[i].src = images[i].getAttribute("data-src");
+  }
+}
+function reload() {
+  const reloadBtn = document.createElement("button");
+  reloadBtn.textContent = "X";
+  document.body.append(reloadBtn);
+  reloadBtn.addEventListener("click", function () {
+    location.reload();
+  });
+}
