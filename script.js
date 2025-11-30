b0207ce28b8cee9c36942baaeb9ce05a68dd4581
Ronit <ronitkbiswas@gmail.com>
2025-11-30 08:49:43 +0530

    added next button

--- script.js
+++ script.js
@@ -5,11 +5,22 @@ function show() {
     images[i].src = images[i].getAttribute("data-src");
   }
 }
-function reload() {
+function closeImg() {
   const reloadBtn = document.createElement("button");
   reloadBtn.textContent = "X";
   document.body.append(reloadBtn);
   reloadBtn.addEventListener("click", function () {
     location.reload();
+  });
+}
+let next = 0;
+function nextImg() {
+  const nextBtn = document.createElement("button");
+  nextBtn.textContent = " >> ";
+  document.body.append(nextBtn);
+  nextBtn.addEventListener("click", function () {
+    const img = document.querySelector("#id1 img");
+    next++;
+    img.src = `https://picsum.photos/400/300?random=${next}`;
   });
 }
