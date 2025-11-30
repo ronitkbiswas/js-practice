8c55ccdb84c690f83a17f73cc132268f64219ae6
Ronit <ronitkbiswas@gmail.com>
2025-11-30 09:42:23 +0530

    updated everything

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ function show() {
   for (let i = 0; i < images.length; i++) {
     images[i].src = images[i].getAttribute("data-src");
   }
+  document.getElementById("btn").style.display = "none";
 }
 function closeImg() {
   const closeBtn = document.createElement("button");
   closeBtn.textContent = " Reset ";
+  closeBtn.style.fontSize = "13px";
+  closeBtn.style.padding = "10px";
+  closeBtn.style.backgroundColor = "red";
+  closeBtn.style.color = "white";
+  closeBtn.style.fontWeight = "bold";
   document.body.append(closeBtn);
   closeBtn.addEventListener("click", function () {
     location.reload();
@@ -17,6 +23,11 @@ let next = 0;
 function nextImg() {
   const nextBtn = document.createElement("button");
   nextBtn.textContent = " next >>  ";
+  nextBtn.style.fontSize = "13px";
+  nextBtn.style.padding = "10px";
+  nextBtn.style.backgroundColor = "green";
+  nextBtn.style.color = "white";
+  nextBtn.style.fontWeight = "bold";
   document.body.append(nextBtn);
   nextBtn.addEventListener("click", function () {
     const img = document.querySelector("#id1 img");
@@ -27,6 +38,11 @@ function nextImg() {
 function prevImg() {
   const prevBtn = document.createElement("button");
   prevBtn.textContent = " << previous ";
+  prevBtn.style.fontSize = "13px";
+  prevBtn.style.padding = "10px";
+  prevBtn.style.backgroundColor = "green";
+  prevBtn.style.color = "white";
+  prevBtn.style.fontWeight = "bold";
   document.body.append(prevBtn);
   prevBtn.addEventListener("click", function () {
     const img = document.querySelector("#id1 img");
